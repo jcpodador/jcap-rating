2e0f06add3594c585d37de6449ac0340ba4a0f50
jaycap <jaypodador@gmail.com>
2021-10-22 17:57:33 +0800

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rating-app'`, () => {
+  it(`should have as title 'presidential-rating'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rating-app');
+    expect(app.title).toEqual('presidential-rating');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rating-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('presidential-rating app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rating-app';
+  title = 'presidential-rating';
+
+  currentRate = 0;
 }
